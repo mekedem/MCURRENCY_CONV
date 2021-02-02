ac266049361c9ca127a037a9145c203d45acf5e0
mekedem <mekedemgetaneh@gmail.com>
2021-02-02 22:25:51 +0300

    added simple widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mcurrency_conv/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('SearchInput', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    var textField = find.byType(TextField);
+    var textval = find.byType(Text);
+    expect(textField, findsOneWidget);
+    await tester.enterText(textField, 'Hello');
+    expect(find.text(' '), findsOneWidget);
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(textval.toString(), '');
+    tester.getSize(textval);
   });
 }
